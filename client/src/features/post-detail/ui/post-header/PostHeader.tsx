1074932a7eb8999e9137a56d57bca44a11b482b1
jee-in <chris309804@gmail.com>
2025-07-15 15:18:45 +0900

    feat: 포스트 업데이트 날짜를 툴팁으로 표시

--- client/src/features/post-detail/ui/post-header/PostHeader.tsx
+++ client/src/features/post-detail/ui/post-header/PostHeader.tsx
@@ -8,17 +8,21 @@ interface PostHeaderProps {
 
 export default function PostHeader({ title, postDate }: PostHeaderProps) {
   const isSame = isSameDate(postDate.createdAt, postDate.updatedAt);
+  const updateMsg = formatUtcToKoreanDate(postDate.updatedAt)
+    ? `${formatUtcToKoreanDate(postDate.updatedAt)} 업데이트`
+    : "등록 이후 업데이트된 포스트입니다.";
 
   return (
     <div className="content-header">
       <h1>{title}</h1>
       <div className="post-heaeder-bottom-box">
-        <span className="post-date">{formatUtcToKoreanDate(postDate.createdAt) ?? ""} 등록</span>
-        {!isSame && (
-          <span className="post-date">
-            {formatUtcToKoreanDate(postDate.updatedAt) ?? ""} 업데이트
-          </span>
-        )}
+        <div className="post-date gap-2">
+          <div>{formatUtcToKoreanDate(postDate.createdAt)}</div>
+          <div>등록</div>
+          <i className="info-icon">
+            <div className="info-tooltip">{isSame ? "최신 글입니다." : updateMsg}</div>
+          </i>
+        </div>
       </div>
     </div>
   );
